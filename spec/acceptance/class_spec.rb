d918337ce35e050d422ec167d5164285bf3f1fa8
Daniel van der Ende <daniel.van.der.ende@ing.nl>
2016-01-07 10:52:16 +0100

    Corrected tests and added CentOS 6 testing

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper_acceptance'
 
-describe 'confluent_kafka class' do
+describe 'kafka class' do
 
   context 'default parameters' do
     # Using puppet_apply as a helper
     it 'should work idempotently with no errors' do
       pp = <<-EOS
-      class { 'confluent_kafka': }
+      class { 'kafka': }
       EOS
 
       # Run it twice and test for idempotency
@@ -14,11 +14,11 @@ describe 'confluent_kafka class' do
       apply_manifest(pp, :catch_changes  => true)
     end
 
-    describe package('confluent_kafka') do
+    describe package('kafka') do
       it { is_expected.to be_installed }
     end
 
-    describe service('confluent_kafka') do
+    describe service('kafka') do
       it { is_expected.to be_enabled }
       it { is_expected.to be_running }
     end
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'confluent_kafka' do
+describe 'kafka' do
   context 'supported operating systems' do
     on_supported_os.each do |os, facts|
       context "on #{os}" do
@@ -8,31 +8,32 @@ describe 'confluent_kafka' do
           facts
         end
 
-        context "confluent_kafka class without any parameters" do
+        context "kafka class without any parameters" do
           let(:params) {{ }}
 
           it { is_expected.to compile.with_all_deps }
 
-          it { is_expected.to contain_class('confluent_kafka::params') }
-          it { is_expected.to contain_class('confluent_kafka::install').that_comes_before('confluent_kafka::config') }
-          it { is_expected.to contain_class('confluent_kafka::config').that_comes_before('confluent_kafka::service') }
-          it { is_expected.to contain_package('confluent-kafka-2.10.4').with_ensure('0.8.2.0-1') }
+          it { is_expected.to contain_class('kafka::params') }
+          it { is_expected.to contain_class('kafka::install').that_comes_before('kafka::config') }
+          it { is_expected.to contain_class('kafka::config').that_comes_before('kafka::service') }
+          # it { is_expected.to contain_package('confluent-kafka-2.10.4').with_ensure('0.8.2.0-1') }
         end
 
-        context "confluent_kafka class with parameters" do
+        context "kafka class with parameters" do
           let(:params) {{
-          :zk_hosts          => ['zk01.example.com:2181','zk02.example.com:2181'],
-          :zk_chroot         => '/chroot',
-          :kafka_config      => { 'port' => 9000 },
-          :brokers           => { 'test01.example.com' => 1 },
-          :app_log_dir       => '/tmp/log/kafka',
-          :log_dirs          => ['/tmp/kafka1'],
-          :install_service   => true,
-          :manage_service    => true,
-          :service_name      => 'kafka',
-          :manage_repo       => true,
-          :jvm_heap_mem      => '-Xmx5G -Xms5G',
-          :restart_on_change => true,
+          :zookeeper_connect  => ['zk01.example.com:2181','zk02.example.com:2181'],
+          :port               => 9000,
+          :app_log_dir        => '/tmp/log/kafka',
+          :log_dirs           => '/tmp/kafka1',
+          :install_service    => true,
+          :manage_service     => true,
+          :service_name       => 'kafka',
+          :manage_repo        => true,
+          :jvm_heap_mem       => '-Xmx5G -Xms5G',
+          :restart_on_change  => true,
+          :reponame           => 'kafka',
+          :repourl            => 'file:///kafka/',
+          :repodescr          => 'descr'
           }}
 
           let (:facts) {
@@ -44,9 +45,9 @@ describe 'confluent_kafka' do
 
           # Ensure ZK string setup properly
           it { should contain_file('/etc/kafka/server.properties') \
-                    .with_content(/zookeeper\.connect=zk01\.example\.com:2181,zk02\.example\.com:2181\/chroot/)}
+                    .with_content(/zookeeper\.connect=zk01\.example\.com:2181,zk02\.example\.com:2181/)}
 
-          it { should contain_file('/etc/kafka/server.properties').that_notifies('Class[confluent_kafka::service]') }
+          it { should contain_file('/etc/kafka/server.properties').that_notifies('Class[kafka::service]') }
 
           # Ensure kafka_config hash gets passed in
           it { should contain_file('/etc/kafka/server.properties') \
@@ -67,7 +68,7 @@ describe 'confluent_kafka' do
           it { should contain_file('/etc/kafka/log4j.properties') \
                     .with_content(/\/tmp\/log\/kafka/)}
 
-          it { should contain_file('/etc/kafka/log4j.properties').that_notifies('Class[confluent_kafka::service]') }
+          it { should contain_file('/etc/kafka/log4j.properties').that_notifies('Class[kafka::service]') }
 
           it { should contain_file('/tmp/kafka1').with({
                         'ensure'  => 'directory',
@@ -91,7 +92,7 @@ describe 'confluent_kafka' do
                         'owner'     => 'kafka',
                         'group'     => 'kafka',
           })}
-          it { should contain_file('/etc/default/kafka').that_notifies('Class[confluent_kafka::service]') }
+          it { should contain_file('/etc/default/kafka').that_notifies('Class[kafka::service]') }
 
 
         end
